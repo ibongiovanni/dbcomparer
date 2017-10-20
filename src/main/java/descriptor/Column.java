6c2910b31fce781669f082777d17601dceb6cd6e
fernando13 <fernandobentolila_13@hotmail.com>
2017-10-20 14:26:33 -0300

    some implementation of  class Column, DataBase, Table

--- src/main/java/descriptor/Column.java
+++ src/main/java/descriptor/Column.java
@@ -2,4 +2,56 @@ package descriptor;
 
 public class Column {
 
+	private String name;
+	private String type;
+	private Table table;
+	
+	
+	public Column(){}
+	
+	public Column(String name, String type){
+		this.name = name;
+		this.type = type;		
+	}
+	
+	
+	public String getName(){
+		return name;
+	}
+	
+	
+	public String getType(){
+		return type;
+	}
+	
+	
+	public Table getTable(){
+		return table;
+	}
+	
+	
+	public void setTable(Table t){
+		table = t;
+	}
+	
+	
+	@Override
+	public boolean equals(Object o){
+		Column col = (Column)o;
+		return (name.equals(col.getName()) && type.equals(col.getType()));
+	}
+	
+	
+	public String compare(Column col){
+		// completar...
+		return "algo";
+	}
+	
+	@Override
+	public String toString(){
+		String s = "Name: " + name +" Type: " + type;
+		return s;
+	}
+	
+
 }
