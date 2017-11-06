cd07f9f93b5b0db911c6536f628e6c73804564df
ibongiovanni <ignaciobongiovanni@gmail.com>
2017-11-06 16:54:17 -0300

    Add main class app

---
+++ src/main/java/App.java
@@ -0,0 +1,53 @@
+
+import utils.*;
+import descriptor.*;
+import java.sql.*;
+import java.util.*;
+
+public class App {
+  // JDBC driver name and database URL
+  static final String JDBC_DRIVER = "org.postgresql.Driver";  
+  static final String DB_URL = "jdbc:postgresql://localhost:5432/postgres";
+
+  //  Database credentials
+  static final String USER = "postgres";
+  static final String PASS = "root"; 
+
+  public static void main(String[] args) {
+    Connection conn = null;
+    try{
+      //STEP 2: Register JDBC driver
+      Class.forName(JDBC_DRIVER);
+
+      //STEP 3: Open a connection
+      System.out.println("Connecting to database...");
+      conn = DriverManager.getConnection(DB_URL,USER,PASS);
+
+      DBMaker test = new DBMaker(conn, "p5e5");
+
+      test.buildDB();
+      DataBase db = test.getDB();
+
+      System.out.println(db);
+      
+      conn.close();
+    }catch(ClassNotFoundException cnfe) {
+      System.err.println("Error loading driver: " + cnfe);
+    }catch(SQLException se){
+        //Handle errors for JDBC
+        se.printStackTrace();
+    }catch(Exception e){
+        //Handle errors for Class.forName
+        e.printStackTrace();
+    }finally{
+      //finally block used to close resources
+      try{
+         if(conn!=null)
+            conn.close();
+      }catch(SQLException se){
+         se.printStackTrace();
+      }//end finally try
+    }//end try
+    System.out.println("GoodBye!");
+  }//end main
+}
