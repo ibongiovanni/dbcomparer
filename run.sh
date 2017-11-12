a957bd438f1a9ec231ee631b650d6a1374822647
ibongiovanni <ignaciobongiovanni@gmail.com>
2017-11-12 17:37:23 -0300

    update scripts

--- run.sh
+++ run.sh
@@ -4,9 +4,6 @@ export CLASSPATH=""
 for file in `ls target/dependency`; do export CLASSPATH=$CLASSPATH:target/dependency/$file; done
 export CLASSPATH=$CLASSPATH:target/classes
 
-echo "$#"
-echo "$1"
-echo "$2"
 if [[ $# -ne 2 ]]; then
   echo "ERROR: Wrong number of params"
   echo "Use: program config_file1 config_file2"
